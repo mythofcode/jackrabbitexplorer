944f2ccf8ea68a3cd27546fbf96789ce7444df20
james.pickup <james.pickup@05f86886-2b44-11de-a1c6-114bfcb2beae>
2010-08-10 04:40:07 +0000

    Initial commit

---
+++ jackrabbitexplorer/src/com/priocept/jcr/client/callback/AddNodeTypesServiceCallback.java
@@ -0,0 +1,23 @@
+package com.priocept.jcr.client.callback;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.priocept.jcr.client.JackrabbitExplorer;
+import com.smartgwt.client.util.SC;
+
+/**
+ * 
+ * @author James Pickup
+ *
+ */
+public class AddNodeTypesServiceCallback implements AsyncCallback<Boolean>
+{
+	public void onSuccess(Boolean result) {
+		SC.say("Added CND node types successfully.");
+		JackrabbitExplorer.hideLoadingImg();		
+	}
+	
+	public void onFailure(Throwable caught) {
+		SC.warn(caught.toString(), new NewBooleanCallback());
+		JackrabbitExplorer.hideLoadingImg();
+	}
+}
